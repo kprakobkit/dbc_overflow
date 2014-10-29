5945663dc7535b5e7145e15a7878176ac2acf344
Peter Prakobkit <kprakobkit@gmail.com>
2014-10-29 12:26:23 -0700

    wrote test and implement functionality to add a new question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,30 @@
 class QuestionsController < ApplicationController
   def index
     @questions = Question.all
+    @question = Question.new
+  end
+
+  def new
+    redirect_to questions_path
   end
 
   def show
     @question = Question.find(params[:id])
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to question_path(@question)
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :content)
   end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -11,4 +11,25 @@ RSpec.describe QuestionsController, :type => :controller do
     get :show, id: question.id
     expect(assigns(:question)).to eq Question.find(question.id)
   end
+
+  context '#new' do
+    it 'should redirect to index' do
+      get :new
+      expect(response).to redirect_to(questions_path)
+    end
+  end
+
+  context '#create' do
+    it 'should create a question with valid params' do
+      expect {
+      post :create, { question: {title: "title", content: "content" } }
+      }.to change { Question.count }.by(1)
+    end
+
+    it 'should create a question with invalid params' do
+      expect {
+      post :create, { question: {title: "", content: "" } }
+      }.to change { Question.count }.by(0)
+    end
+  end
 end
