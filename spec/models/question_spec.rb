01c9230007c6cff3df7cf605956e9b681dccaa06
Peter Prakobkit <kprakobkit@gmail.com>
2014-10-29 11:09:29 -0700

    test setup and wrote validation tests for question and answer models

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Question, :type => :model do
+  describe "validates presence of title and content" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:content) }
+  end
+end
