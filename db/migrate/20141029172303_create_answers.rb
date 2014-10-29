7279e7d02d11dd20c968431869b18617735f05b7
Peter Prakobkit <kprakobkit@gmail.com>
2014-10-29 10:26:30 -0700

    set route to question#index, generate models, and migrations

---
+++ db/migrate/20141029172303_create_answers.rb
@@ -0,0 +1,11 @@
+class CreateAnswers < ActiveRecord::Migration
+  def change
+    create_table :answers do |t|
+      t.string :title
+      t.string :content
+      t.references :question, index: true
+
+      t.timestamps
+    end
+  end
+end
