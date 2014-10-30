da4127d4075e6ac6de5147c26034f0f196a5b2be
Peter Prakobkit <kprakobkit@gmail.com>
2014-10-30 08:57:47 -0700

    commit while in the process getting ajax sorting to work

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,7 @@
 class Quote < ActiveRecord::Base
   def self.get_quote
-   response = HTTParty.get "https://api.github.com/zen", :headers=> { "User-Agent" => ENV['username'] }
-   response.body
+   # response = HTTParty.get "https://api.github.com/zen", :headers=> { "User-Agent" => ENV['username'] }
+   # response.body
+  response = "temp stub"
   end
 end
