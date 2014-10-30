f51dfd5a0811d09479c2e5beeffa157758cd9fbe
Peter Prakobkit <kprakobkit@gmail.com>
2014-10-30 16:50:35 -0700

    refactor

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,7 +1,6 @@
 class Quote < ActiveRecord::Base
   def self.get_quote
-   # response = HTTParty.get "https://api.github.com/zen", :headers=> { "User-Agent" => ENV['username'] }
-   # response.body
-  response = "temp stub"
+    response = HTTParty.get "https://api.github.com/zen", :headers=> { "User-Agent" => ENV['username'] }
+    response.body
   end
 end
