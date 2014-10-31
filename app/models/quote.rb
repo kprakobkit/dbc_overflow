1f07aa7cb53dc5cbb7e53f5957bb444f8b507e43
Peter Prakobkit <kprakobkit@gmail.com>
2014-10-30 19:15:22 -0700

    fix issue with sorting

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,6 @@
 class Quote < ActiveRecord::Base
   def self.get_quote
-    response = HTTParty.get "https://api.github.com/zen", :headers=> { "User-Agent" => ENV['username'] }
+    response = HTTParty.get "https://api.github.com/zen", :headers=> { "User-Agent" => ENV['username'], "Authorization" => "token #{ENV['token']}" }
     response.body
   end
 end
